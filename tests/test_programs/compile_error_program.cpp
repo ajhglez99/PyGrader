41fba90137a77a516afdf5ca927e0b7599dc9bf6
ajhglez99 <ajhglez99@gmail.com>
2025-01-02 21:12:54 -0300

    Add a program with intentional syntax error for compilation testing

---
+++ tests/test_programs/compile_error_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * @file compile_error_program.cpp
+ * @brief This program is intentionally written with a syntax error for testing purposes.
+ *
+ * The program lacks a semicolon after the return statement in the main function.
+ * This is done to test if the package can properly detect compilation failures.
+ */
+
+int main()
+{
+    return 0 
+}
